a01e0a67ae57e48fbc53c9b30322a7372213b8aa
SABRINA FERNANDES DRAGONETI <sabrina.dragoneti2@senaisp.edu.br>
2026-03-20 09:16:13 -0300

    docs para atualização dos comentarios

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-let inputTarefa = document.getElementById("inputTarefa")
-let listaTarefas = document.getElementById("listaTarefas");
-let btnAdicionar = document.getElementById("btnAdicionar");
-let mensagem = document.getElementById("mensagem");
+let inputTarefa = document.getElementById("inputTarefa") // pega o campo onde digita a tarefa
+let listaTarefas = document.getElementById("listaTarefas"); // pega a lista de tarefas
+let btnAdicionar = document.getElementById("btnAdicionar"); // pega o botão adicionar
+let mensagem = document.getElementById("mensagem"); // pega o local onde aparece a mensagem
 
-btnAdicionar.addEventListener("click", () => {
+btnAdicionar.addEventListener("click", () => { // quando clicar no botão
 
-    let texto = inputTarefa.value
+    let texto = inputTarefa.value // pega o que foi digitado
 
-    if (texto === " ") {
-        mensagem.innerText = "Tarefa vazia";
-        mensagem.classList.remove("text-sucess");
-        mensagem.classList.add("text-danger")
+    if (texto === " ") { // verifica se está vazio (apenas espaço)
+        mensagem.innerText = "Tarefa vazia"; // mostra mensagem de erro
+        mensagem.classList.remove("text-sucess"); // remove classe de sucesso
+        mensagem.classList.add("text-danger") // adiciona classe de erro (vermelho)
 
     } else {
-        let li = document.createElement("li");
-        li.innerText = texto;
-        li.classList.add("list-group-item");
+        let li = document.createElement("li"); // cria um item da lista
+        li.innerText = texto; // coloca o texto digitado
+        li.classList.add("list-group-item"); // adiciona estilo
 
-        listaTarefas.appendChild(li);
+        listaTarefas.appendChild(li); // adiciona na lista
         
-        mensagem.innerText = "Tarefa adicionada";
-        mensagem.classList.remove("text-sucess");
-        mensagem.classList.add("text-danger")
+        mensagem.innerText = "Tarefa adicionada"; // mensagem de sucesso
+        mensagem.classList.remove("text-sucess"); // remove classe de sucesso
+        mensagem.classList.add("text-danger") // adiciona classe (aqui está vermelho também)
     }
 
 });
