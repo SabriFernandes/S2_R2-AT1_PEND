a1eca6319ba89adafe697f009dbaa43db2dc28f0
SABRINA FERNANDES DRAGONETI <sabrina.dragoneti2@senaisp.edu.br>
2026-03-20 09:28:06 -0300

    docs atualização dos comentarios

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let nomeUsuario = document.getElementById("nomeUsuario");
-let mensagem = document.getElementById("mensagem");
-let btnCadastrar = document.getElementById("btnCadastrar");
-let nome
+let nomeUsuario = document.getElementById("nomeUsuario"); // pega o campo onde o usuário digita o nome
+let mensagem = document.getElementById("mensagem"); // pega o lugar onde a mensagem vai aparecer
+let btnCadastrar = document.getElementById("btnCadastrar"); // pega o botão
+let nome // variável para guardar o nome
 
-btnCadastrar.addEventListener("click", () => {
+btnCadastrar.addEventListener("click", () => { // quando clicar no botão
 
-    nome = nomeUsuario.value;
+    nome = nomeUsuario.value; // pega o nome digitado
 
-    if (nome) {
-        mensagem.innerText = `Olá ${nome}, seja bem-vindo!`;
+    if (nome) { // verifica se foi digitado algo
+        mensagem.innerText = `Olá ${nome}, seja bem-vindo!`; // mostra mensagem com o nome
     }
 });
