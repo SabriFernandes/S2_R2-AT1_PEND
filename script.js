30d329ed64771d4aba32ed161c6e984d35cdf231
SABRINA FERNANDES DRAGONETI <sabrina.dragoneti2@senaisp.edu.br>
2026-03-20 09:22:14 -0300

    docs atualização dos comentarios

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let mensagem = document.getElementById("mensagem");
-let btnTrocar = document.getElementById("btnTrocar");
+let mensagem = document.getElementById("mensagem"); // pega o elemento onde a mensagem vai aparecer
+let btnTrocar = document.getElementById("btnTrocar"); // pega o botão
 
-
-btnTrocar.addEventListener("click", () => {
- mensagem.innerText = "Mensagem trocada";
+btnTrocar.addEventListener("click", () => { // quando clicar no botão
+ mensagem.innerText = "Mensagem trocada"; // muda o texto da mensagem
     
 });
